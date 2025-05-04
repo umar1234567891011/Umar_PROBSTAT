f14de0a6d95dfd4492fe8d10a73f77d7ce94d94c
umar1234567891011 <vilogumar@gmail>
2025-05-04 11:03:42 +0700

    tambah file index.php untuk menampilkan nama

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+$name = "Umar";
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Awal</title>
+</head>
+<body>
+    <h1>Halo nama saya <?php echo $name?> </h1>
+
+</body>
+</html>
